585744dcfe3dde4ac5c10c4cd2eee7064d9b37fc
Reuben Bell <reuben_bell@hotmail.com>
2013-07-24 04:36:47 +1200

    Piece Classifier now classifys edges as IN/OUT/FLAT

--- PieceClassifier.cpp
+++ PieceClassifier.cpp
@@ -2,6 +2,7 @@
 #include "opencv2/highgui/highgui.hpp"
 
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -23,7 +24,21 @@ using namespace cv;
 Point origin_point(vector<Point>& edge);
 double interior_angle(Point middle_vertex, Point prev_vertex, Point next_vertex);
 
-void display(PieceData &piece, string window_name, Point origin, vector<Point> &smoothed_edge, vector<Point> &corner_points)
+void drawLineList(Mat display_img, list<Point>& point_list, Scalar color, int line_width)
+{
+	list<Point>::iterator it = point_list.begin();
+	Point* prev_point = &*it;
+
+	while(++it != point_list.end())
+	{
+		Point* current_point = &*it;
+		line(display_img, *prev_point, *current_point, color, line_width);
+
+		prev_point = current_point;
+	}
+}
+
+void display(PieceData &piece, string window_name, Point origin, vector<Point> &smoothed_edge, vector<int> &corner_points, vector< list<Point> > &individual_edges)
 {
 	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
 
@@ -32,12 +47,16 @@ void display(PieceData &piece, string window_name, Point origin, vector<Point> &
 	display_img.create(raw_img.size(), raw_img.type());
 	raw_img.copyTo(display_img);
 
-	vector< vector<Point> > contours;
+	/*vector< vector<Point> > contours;
 	contours.push_back(piece.edge());
 	contours.push_back(smoothed_edge);
-
 	drawContours(display_img, contours, 0, Scalar(0, 0, 255), 2);
-	drawContours(display_img, contours, 1, Scalar(0, 255, 0), 2);
+	drawContours(display_img, contours, 1, Scalar(255, 0, 0), 2);*/
+
+	drawLineList(display_img, individual_edges[0], Scalar(128, 128, 0), 2);
+	drawLineList(display_img, individual_edges[1], Scalar(0, 255, 0), 2);
+	drawLineList(display_img, individual_edges[2], Scalar(0, 0, 255), 2);
+	drawLineList(display_img, individual_edges[3], Scalar(255, 255, 210), 2);
 
 	circle(display_img, origin, 5, Scalar(0, 255, 0), -1);
 
@@ -48,7 +67,7 @@ void display(PieceData &piece, string window_name, Point origin, vector<Point> &
 
 	for (int i = 0; i < corner_points.size(); i++) 
 	{
-		circle(display_img, corner_points[i], 6, Scalar(128, 0, 255), -1);
+		circle(display_img, piece.edge()[corner_points[i]], 6, Scalar(128, 0, 255), -1);
 	}
 
 
@@ -111,7 +130,7 @@ Point origin_point(vector<Point>& edge)
 	return Point (point_x, point_y);
 }
 
-vector<Point> find_critical_points(vector<Point>& smoothed_edge, Point origin)
+vector<Point> find_corner_points(vector<Point>& smoothed_edge, Point origin)
 {
 	double greatest_area = 0;
 	vector<Point> corner_points (4);
@@ -162,6 +181,110 @@ vector<Point> find_critical_points(vector<Point>& smoothed_edge, Point origin)
 	return corner_points;
 }
 
+vector<int> find_corner_indexs(vector<Point>& edge, vector<Point>& corner_points)
+{
+	vector<int> corner_indexs;
+	
+	for (int i = 0; i < corner_points.size(); i++)
+	{
+		for (int j = 0; j < edge.size(); j++)
+		{
+			if (corner_points[i] == edge[j])
+			{
+				corner_indexs.push_back(j);
+				break;
+			}
+		}
+	}
+
+	return corner_indexs;
+}
+
+vector< list<Point> > split_edges(vector<Point>& edge, vector<int>& corner_indexs)
+{
+	vector< list<Point> > individual_edges (4);
+
+	int edge_index = 0;
+	bool insert_end = true;
+	list<Point>::iterator it;
+	for (int i = 0; i < edge.size(); i++) 
+	{
+		if (insert_end)
+		{
+			individual_edges[edge_index].push_back(edge[i]);
+		}
+		else
+		{
+			individual_edges[edge_index].insert(it, edge[i]);
+		}
+
+
+		if (i == corner_indexs[edge_index])
+		{
+			edge_index = (edge_index + 1) % 4;
+			if (edge_index == 0)
+			{
+				insert_end = false;
+				it = individual_edges[edge_index].begin();
+			}
+
+			i--;
+		}
+	}
+	
+	return individual_edges;
+}
+
+double distance_from_line(double cx, double cy, double ax, double ay, double bx, double by)
+{
+	double r_numerator = (cx-ax)*(bx-ax) + (cy-ay)*(by-ay);
+	double r_denomenator = (bx-ax)*(bx-ax) + (by-ay)*(by-ay);
+	double r = r_numerator / r_denomenator;
+   	double px = ax + r*(bx-ax);
+  	double py = ay + r*(by-ay);
+    	double s =  ((ay-cy)*(bx-ax)-(ax-cx)*(by-ay) ) / r_denomenator;
+
+	return fabs(s)*sqrt(r_denomenator);
+}
+
+double distance_from_line(Point p, Point l1, Point l2)
+{
+	return distance_from_line(p.x, p.y, l1.x, l1.y, l2.x, l2.y);
+}
+
+bool side_of_line(Point a, Point b, Point c)
+{
+     return ((b.x - a.x)*(c.y - a.y) - (b.y - a.y)*(c.x - a.x)) > 0;
+}
+
+int classify_edge(list<Point> edge, Point origin)
+{
+	Point first_corner = edge.front();
+	Point second_corner = edge.back();
+	list<Point>::iterator it;
+	
+	bool origin_side_of_line = side_of_line(origin, first_corner, second_corner);
+	
+	for(it = edge.begin(); it != edge.end(); it++) 
+	{
+		if (distance_from_line(*it, first_corner, second_corner) > 20) 
+		{
+			bool side = side_of_line(*it, first_corner, second_corner);
+			
+			if (side == origin_side_of_line)  
+				cout << "IN" << endl;
+			else
+				cout << "OUT" << endl;
+
+			return 1;
+		}
+	}
+
+	cout << "FLAT" << endl;
+
+	return 0;
+}
+
 int main(int argc, char* argv[]) 
 {
 	string piece_filename = string(argv[1]);
@@ -175,14 +298,26 @@ int main(int argc, char* argv[])
 
 	Point origin = origin_point(smoothed_edge);
 
-	vector<Point> corner_points = find_critical_points(smoothed_edge, origin);
+	vector<Point> corner_points = find_corner_points(smoothed_edge, origin);
 
 	if (corner_points.size() == 0)
 	{
 		cout << "Does not appear to be valid piece" << endl;
+		return EXIT_FAILURE;
 	}
 
-	display(pd, piece_filename, origin, smoothed_edge, corner_points);
+	vector<int> corner_indexs = find_corner_indexs(edge, corner_points);
+
+	sort(corner_indexs.begin(), corner_indexs.end());
+	
+	vector< list<Point> > individual_edges = split_edges(edge, corner_indexs);
+	
+	for (int i = 0; i < individual_edges.size(); i++) 
+	{
+		classify_edge(individual_edges[i], origin);
+	}
+
+	display(pd, piece_filename, origin, smoothed_edge, corner_indexs, individual_edges);
 
 	waitKey();
 
